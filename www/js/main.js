8d5e2979bf235126b22ca676b84e6457fd8d2c16
Prokop <prokop.tomisek@seznam.cz>
2025-04-25 09:28:52 +0200

    Updates.

--- www/js/main.js
+++ www/js/main.js
@@ -51,24 +51,6 @@
             $dropdown.off("mouseenter mouseleave");
         }
     });
-    
-    
-    // Back to top button
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 100) {
-            $('.back-to-top').fadeIn('slow');
-        } else {
-            $('.back-to-top').fadeOut('slow');
-        }
-    });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
-        return false;
-    });
-
-    // // Image comparison
-    // $(".twentytwenty-container").twentytwenty({});
-
 
     // Price carousel
     $(".price-carousel").owlCarousel({
